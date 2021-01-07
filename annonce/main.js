fe366a4ce4816af669565c73085751b6ed916984
nellyam <nellynellyam@gmail.com>
2021-01-07 11:02:46 +0100

    créer la main.js

---
+++ annonce/main.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/***************************
+ * VARIABLES
+ ****************************/
+
+let photos = [ 
+    {src: "canicheScroll.png"}, 
+    {src: "2.jpg"},
+    {src: "3.jpg"},
+    {src: "4.jpg"},
+    {src: "5.jpg"},
+    {src: "6.jpg"}
+    ];
+
+ let prevButton = document.querySelector("article #previous");
+ let nextButton = document.querySelector("article #next");
+ let image = document.querySelector("article #slide");
+
+
+/***************************
+ * FONCTIONS
+ ****************************/
+ function next() {
+     let i = 0;
+
+ }
+
+ function previous() {}
